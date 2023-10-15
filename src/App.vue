f7460fea25cb37e4088d8929c5853e6d8db6fe3a
Daniel Merlo <daniellmerlo@gmail.com>
2023-10-15 14:11:27 -0300

    Projeto em: Declarative Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
+<script setup>
+import {ref, reactive} from 'vue'
+
+const message = ref('Hello World!')
+const counter = reactive({
+  count:0
+})
+
+console.log(message.value)
+message.value = 'Changed'
+
+console.log(counter.count)
+counter.count++
+
+</script>
 <template>
-  <h1>Hello World!</h1>
+  <h1>{{ message }}</h1>
+  <p>count is: {{ counter.count }}</p>
+  <h1>{{ message.split('').reverse().join() }}</h1>
 </template>
