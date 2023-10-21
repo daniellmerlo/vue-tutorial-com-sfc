e031c7bd62b0750ecd7169947a735ba6163712ae
Daniel Merlo <daniellmerlo@gmail.com>
2023-10-21 06:57:22 -0300

    Projeto em: Props

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
+import { ref } from 'vue'
 import ChildComp from './ChildComp.vue'
+
+const greetings = ref('Hello from parent')
 </script>
 <template>
-  <ChildComp />
+  <ChildComp :msg="greetings" />
 </template>
